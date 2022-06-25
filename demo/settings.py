e6b1f3e896e5428de25cec36112b3680101435d6
DeLL <krishnabk651@gmail.com>
2022-06-25 22:40:01 +0545

    commit

--- demo/settings.py
+++ demo/settings.py
@@ -134,6 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,"static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 
 # Default primary key field type
