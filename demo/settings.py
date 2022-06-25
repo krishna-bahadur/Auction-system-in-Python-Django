255c405cce8bfb0e2c208646b48091286af257a3
DeLL <krishnabk651@gmail.com>
2022-06-25 22:04:44 +0545

    commit

--- demo/settings.py
+++ demo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-t!!e$1vez5%726%_8enuj5w$kf&v96lf7=nf=7x+=vidtxg^be
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hamroauctionsystem.herokuapp.com']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
