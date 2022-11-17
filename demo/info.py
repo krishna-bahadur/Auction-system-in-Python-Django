397f8e24a2affa7f2715e45265d9a19a335da4fb
Krishna Bahadur BK <85353641+krishna-bahadur@users.noreply.github.com>
2022-11-17 23:25:45 +0545

    Update info.py
    Change

--- demo/info.py
+++ demo/info.py
@@ -2,5 +2,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hamroauctionsystem@gmail.com'
-EMAIL_HOST_PASSWORD ='rqvuhmfeozcztgaj'
+EMAIL_HOST_PASSWORD ='testpassword'
 EMAIL_PORT = 587
